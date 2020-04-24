299bad2e112563cce81cd2a070c49efdae5dc0f6
18016549 <roy1409@outlook.sg>
2020-04-24 12:12:45 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demoscrollview/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.demoscrollview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
